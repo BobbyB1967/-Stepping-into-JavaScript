765da89f68eeaf2519fc373822b87a3040bc28fe
RobertBright <BobbyB1967@gmail.com>
2018-02-12 18:39:10 -0500

    checkpoint challenge 2

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-function isPrime (num){
 
-for(i=2; i<num; i++){
-if (num%i===0)
-return false;}
 
-return true;
+var BirthYear;
+var CurrentYear;
+
+calc(2018,1990)
+
+function calc(CurrentYear,BirthYear){
+
+var age = CurrentYear - BirthYear;
+if (age > 21){
+
+document.write( "you are " + age + " years of age, drink up dude")
+console.log( "you are " + age + " years of age, drink up dude")
+}else {
+  document.getElementById("ch11").innerHTML="You can not drink at your age";
+  document.write("you are only " + age + " Sorry you can not drink" )
+  console.log("you are only " + age + " Sorry you can not drink" )
 }
-console.log (isPrime(598));
+
+}
