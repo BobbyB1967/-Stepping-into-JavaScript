8bf26cd051d9fd9d7122dedeaba3fa4fbdcb0bf0
RobertBright <BobbyB1967@gmail.com>
2018-02-14 12:26:18 -0500

    checkpoint challenge 6 writing to divs with js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 
-myFunction();
-function myFunction() {
-    var str = "Javascript will make you lose your mind";
-    var res = str.split(" ");
-    document.write(res);
-    console.log(res);
-  }
+
+
+document.getElementById("myChatter").innerHTML =
+"<h1>This is an example using an h1 tag to write to  a div named myChatter</h1>";
+
+document.getElementById("myChatter1").innerHTML =
+"This is an example of writng to another div with normal text";
+
+document.getElementById('myChatter1').innerHTML = "an example of writing to a nested div";
