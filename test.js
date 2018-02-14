cdd5f598599ee5ef1b6470cb2d8bf6c7b31d7301
RobertBright <BobbyB1967@gmail.com>
2018-02-13 21:20:31 -0500

    checkpoint challenge 2

--- test.js
+++ test.js
@@ -1,21 +1,9 @@
 
 
-var BirthYear;
-var CurrentYear;
+var age = 1;
+var text = "your 50";
 
-calc(2018,1990)
-
-function calc(CurrentYear,BirthYear){
-
-var age = CurrentYear - BirthYear;
-if (age > 21){
-
-document.write( "you are " + age + " years of age, drink up dude")
-console.log( "you are " + age + " years of age, drink up dude")
-}else {
-  document.getElementById("ch11").innerHTML="You can not drink at your age";
-  document.write("you are only " + age + " Sorry you can not drink" )
-  console.log("you are only " + age + " Sorry you can not drink" )
-}
-
+while (age < 50){
+console.log("You are not 50 years of age");
+  age++;
 }
