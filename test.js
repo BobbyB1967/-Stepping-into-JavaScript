9ce679ecbc7c6a834d3c6a0b6698e7c3a5cda906
RobertBright <BobbyB1967@gmail.com>
2018-02-14 00:28:26 -0500

    checkpoint challenge 5

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-var t1 = Boolean("Learning");
-var t2 = Boolean("Js");
-var t3 = 1;
-while (t3 < 25){
-console.log(Boolean("Learning"));
 
-t3++;
-}
-document.getElementById("ch11").innerHTML = Boolean("js");
+myFunction();
+function myFunction() {
+    var str = "Javascript will make you lose your mind";
+    var res = str.split(" ");
+    document.write(res);
+    console.log(res);
+  }
